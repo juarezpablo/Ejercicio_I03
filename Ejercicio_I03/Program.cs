3f5279f65f3992f1adb53d0307007ee852556deb
Pablo-notebook <juarezjpablo7@gmail.com>
2023-09-10 13:49:52 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio_I03/Program.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+namespace Ejercicio_I03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new Estudiante("silva","1345","pepito");
+            Estudiante estudiante2 = new Estudiante("ruiz", "13456", "jose");
+            Estudiante estudiante3 = new Estudiante("tere", "134578", "jorge");
+            estudiante1.SetNotaPrimerParcial(6);
+            estudiante1.SetNotaSegundoParcial(7);
+            estudiante2.SetNotaPrimerParcial(8);
+            estudiante2.SetNotaSegundoParcial(9);
+            estudiante3.SetNotaPrimerParcial(2);
+            estudiante3.SetNotaSegundoParcial(4);
+            Console.WriteLine(estudiante1.Mostrar());
+            Console.WriteLine(estudiante2.Mostrar());
+            Console.WriteLine(estudiante3.Mostrar());
+        }
+    }
+}
---
+++ Entidades/Estudiante.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+namespace Entidades
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            Estudiante.random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void SetNotaPrimerParcial(int notaPrimerParcial)
+        {
+            this.notaPrimerParcial = notaPrimerParcial;
+        }
+        public void SetNotaSegundoParcial(int notaSegundoParcial)
+        {
+            this.notaSegundoParcial = notaSegundoParcial;
+        }
+
+        private float CalcularPromedio()
+        {
+            float promedio = (this.notaPrimerParcial + this.notaSegundoParcial) / 2;
+            return promedio;
+        }
+
+        public int CalcularNotaFinal()
+        {
+            if (this.notaPrimerParcial >= 4 && this.notaSegundoParcial >= 4)
+            {
+                return random.Next(6, 11);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            float promedio = CalcularPromedio();
+            int notaFinal = CalcularNotaFinal();
+            sb.AppendLine($"Nombre: {this.nombre}");
+            sb.AppendLine($"Apellido: {this.apellido}");
+            sb.AppendLine($"Legajo: {this.legajo}");
+            sb.AppendLine($"Promedio: {promedio}");
+            if(notaFinal!= -1)
+            {
+                sb.AppendLine($"Nota Final: {notaFinal}");
+            }
+            else
+            {
+                sb.AppendLine("Alumno Desaprobado");
+            }
+            return sb.ToString();
+         }    
+        
+    }
+}
